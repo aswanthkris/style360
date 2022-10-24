c042ae99969abb595310275d4c3cbbf084d4d0ed
Aswanth Krishna <aswanthkris@gmail.com>
2022-10-24 19:30:02 +0530

    local server added

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports={
+
+    ServiceID:"VAc05e7f3ce233f3368f21fb31c90d6c79",
+    accountSID:"ACa02277c7d900998c0f05e647b30ab015",
+    authTocken:"f24f8944a7f5c7cbb719d133c24911e1"
+
+}
+
